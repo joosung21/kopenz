e083b7842baffbb90aef808006b944060737f18e
Jay Ahn <joosung21@gmail.com>
2017-03-14 19:00:49 +0900

    Save

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var app = express()
 var pug = require('pug')
+var bodyParser = require('body-parser')
 app.set('views', './views')
 app.set('view engine', 'pug')
 app.locals.pretty = true
@@ -8,9 +9,27 @@ app.use('/', express.static('public'))
 app.use('/css/', express.static('css'))
 app.use('/js/', express.static('js'))
 app.use('/fonts/', express.static('fonts'))
+app.use('/vendor/', express.static('vendor'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// Temporary DATA
+var pension = require( "./temp_pension_data.js")
+var pension = pension.getlist()
 
 app.get('/', function (req, res) {
-  res.render('index')
+  res.render('index',{
+    pension: pension
+  })
+})
+app.get('/member', function (req, res) {
+  res.render('member')
+})
+app.get('/channel', function (req, res) {
+  res.render('channel')
+})
+app.get('/map', function (req, res) {
+  res.render('map')
 })
 
 app.listen(3000, function () {
---
+++ temp_pension_data.js
@@ -0,0 +1,38 @@
+module.exports = {
+  getlist: function () {
+    return list
+  }
+}
+
+var list = [
+  {title:'우리집펜션',address:'제주 서귀포시',area:'제주',thumbnail:'http://www.pensionworld.net/admin/upload/UPSO_sunup/pension/base/20150428114659121801.jpg',link:'http://www.pensionworld.net/pindex.php?mbId=sunup'},
+  {title:'소울메이트펜션',address:'경기 가평',area:'가평',thumbnail:'http://www.pensionworld.net/admin/upload/UPSO_rose3254/pension/base/20160812165154925664.jpg',link:'http://www.pensionworld.net/pindex.php?mbId=rose3254' },
+  {title:'행복으로의초대',address:'충남 태안군',area:'안면도',thumbnail: 'http://www.pensionworld.net/admin/upload/UPSO_ha/pension/base/20150429101023308070.jpg',link:'http://www.pensionworld.net/pindex.php?mbId=ha'},
+  {title:'썬플라워펜션',address:'강원 통영군',area:'통영',thumbnail:'http://www.pensionworld.net/admin/upload/UPSO_sun01/pension/base/20150417152456049817.jpg',link:'http://www.pensionworld.net/pindex.php?mbId=sun01'},
+  {title:'신두리노을바다펜션',address:'전북 부안군',area:'변산반도',thumbnail:'http://www.pensionworld.net/admin/upload/UPSO_sinduri/pension/base/20160402115548230965.jpg',link:'http://www.pensionworld.net/pindex.php?mbId=sun01'},
+  {title:'휴펜션',address:'제주 서귀포시',area:'제주',thumbnail:'http://www.pensionworld.net/admin/upload/UPSO_gethompy/pension/base/20160509142919115954.jpg',link:'http://www.pensionworld.net/pindex.php?mbId=sunup'},
+  {title:'노을언덕펜션',address:'경기 가평',area:'가평',thumbnail:'http://www.pensionworld.net/admin/upload/UPSO_bada12/pension/base/20160316134809999948.jpg',link:'http://www.pensionworld.net/pindex.php?mbId=rose3254'},
+  {title:'해미읍성들꽃펜션',address:'충남 태안군',area:'안면도',thumbnail:'http://www.pensionworld.net/admin/upload/UPSO_hflower/pension/base/20151027160131572351.jpg',link:'http://www.pensionworld.net/pindex.php?mbId=ha'},
+  {title:'휴양림은하수펜션',address:'강원 통영군',area:'통영',thumbnail:'http://www.pensionworld.net/admin/upload/UPSO_eeee/pension/base/20150429161935988281.jpg',link: 'http://www.pensionworld.net/pindex.php?mbId=sun01'},
+  {title: '꽃지로즈마리',address:'전북 부안군',area:'변산반도',thumbnail: 'http://www.pensionworld.net/admin/upload/UPSO_rosemary/pension/base/20160427135204348153.jpg',link:'http://www.pensionworld.net/pindex.php?mbId=sun01'},
+  {title:'소나무가있는풍경',address:'제주 서귀포시',area:'제주',thumbnail:'http://www.pensionworld.net/admin/upload/UPSO_74086/pension/base/20150410151106034108.jpg',link:'http://www.pensionworld.net/pindex.php?mbId=sunup'},
+  {title:'캐빈하우스',address:'경기 가평',area:'가평',thumbnail:'http://www.pensionworld.net/admin/upload/UPSO_cabin123/pension/base/20151209153729745843.jpg',link:'http://www.pensionworld.net/pindex.php?mbId=rose3254'},
+  {title:'안면도바닷가웨이브',address:'충남 태안군',area:'안면도',thumbnail:'http://www.pensionworld.net/admin/upload/UPSO_wave/pension/base/20150901160649931311.jpg',link:'http://www.pensionworld.net/pindex.php?mbId=ha'},
+  {title:'시온성별장',address:'강원 통영군',area:'통영',thumbnail:'http://www.pensionworld.net/admin/upload/UPSO_sionvilla/pension/base/20160616113840723589.jpg',link: 'http://www.pensionworld.net/pindex.php?mbId=sun01'},
+  {title: '조이랜드펜션',address:'전북 부안군',area:'변산반도',thumbnail: 'http://www.pensionworld.net/admin/upload/UPSO_joy5477/pension/base/20160126150404199942.jpg',link:'http://www.pensionworld.net/pindex.php?mbId=sun01'},
+  {title:'안면도풀하우스',address:'제주 서귀포시',area:'제주',thumbnail:'http://www.pensionworld.net/admin/upload/UPSO_fu7/pension/base/20151207135156828160.jpg',link:'http://www.pensionworld.net/pindex.php?mbId=sunup'},
+  {title:'노을과바다',address:'경기 가평',area:'가평',thumbnail:'http://www.pensionworld.net/admin/upload/UPSO_noelsea/pension/base/20160114155553049903.jpg',link:'http://www.pensionworld.net/pindex.php?mbId=rose3254'},
+  {title:'힐하우스',address:'충남 태안군',area:'안면도',thumbnail:'http://www.pensionworld.net/admin/upload/UPSO_hill12/pension/base/20100813181106193528.jpg',link:'http://www.pensionworld.net/pindex.php?mbId=ha'},
+  {title:'몽산포다솔펜션',address:'강원 통영군',area:'통영',thumbnail:'http://www.pensionworld.net/admin/upload/UPSO_dasol/pension/base/20150325103844262337.jpg',link: 'http://www.pensionworld.net/pindex.php?mbId=sun01'},
+  {title: '수정펜션',address:'전북 부안군',area:'변산반도',thumbnail: 'http://www.pensionworld.net/admin/upload/UPSO_sjung/pension/base/20150415151802791669.jpg',link:'http://www.pensionworld.net/pindex.php?mbId=sun01'},
+  {title:'조약돌펜션',address:'제주 서귀포시',area:'제주',thumbnail:'http://www.pensionworld.net/admin/upload/UPSO_pe123/pension/base/20160223142413275687.jpg',link:'http://www.pensionworld.net/pindex.php?mbId=sunup'},
+  {title:'천리포연일콘도',address:'경기 가평',area:'가평',thumbnail:'http://www.pensionworld.net/admin/upload/UPSO_ye3303/pension/base/20160201134152918532.jpg',link:'http://www.pensionworld.net/pindex.php?mbId=rose3254'},
+  {title:'파스텔펜션',address:'충남 태안군',area:'안면도',thumbnail:'http://www.pensionworld.net/admin/upload/UPSO_pastel02/pension/base/20160527102031835005.jpg',link:'http://www.pensionworld.net/pindex.php?mbId=ha'},
+  {title:'가경주펜션',address:'강원 통영군',area:'통영',thumbnail:'http://www.pensionworld.net/admin/upload/UPSO_beauty7/pension/base/20160407152348263140.jpg',link: 'http://www.pensionworld.net/pindex.php?mbId=sun01'},
+  {title: '옛집황토방펜션',address:'전북 부안군',area:'변산반도',thumbnail: 'http://www.pensionworld.net/admin/upload/UPSO_hwang/pension/base/20160429095735129820.jpg',link:'http://www.pensionworld.net/pindex.php?mbId=sun01'},
+  {title:'석양의추억',address:'제주 서귀포시',area:'제주',thumbnail:'http://www.pensionworld.net/admin/upload/UPSO_mm4114/pension/base/20160126201921146063.jpg',link:'http://www.pensionworld.net/pindex.php?mbId=sunup'},
+  {title:'아름드리펜션',address:'경기 가평',area:'가평',thumbnail:'http://www.pensionworld.net/admin/upload/UPSO_armdri02/pension/base/20160411132414677322.jpg',link:'http://www.pensionworld.net/pindex.php?mbId=rose3254'},
+  {title:'파인베르크',address:'충남 태안군',area:'안면도',thumbnail:'http://www.pensionworld.net/admin/upload/UPSO_pine/pension/base/20160601140603681208.jpg',link:'http://www.pensionworld.net/pindex.php?mbId=ha'},
+  {title:'펜션톰과제리',address:'강원 통영군',area:'통영',thumbnail:'http://www.pensionworld.net/admin/upload/UPSO_grim12/pension/base/20150424150615722565.jpg',link: 'http://www.pensionworld.net/pindex.php?mbId=sun01'},
+  {title: '스머프펜션',address:'전북 부안군',area:'변산반도',thumbnail: 'http://www.pensionworld.net/admin/upload/UPSO_smurf/pension/base/20150326105226762676.jpg',link:'http://www.pensionworld.net/pindex.php?mbId=sun01'},
+]
